bf8cc4c08da0198cb07a973f1760786d0f479adb
0xnamelessDev <javierollersanchez@gmail.com>
2024-03-22 20:06:05 +0100

    calculator 1.0

--- script.js
+++ script.js
@@ -11,56 +11,110 @@ class Calculator{
     //Método para restaurar todos los valores a su estado inicial
     resetearVariables(){
         //Resetear los dos operandos y el operador
-        this.operand1 = 0;
-        this.operand2 = 0;
+        this.operand1 = '0';    //Es el operando superior de menor tamaño
+        this.operand2 = '0';    //Es el operando inferior de mayor tamaño
         this.operator = '';
+        this.result = '';
+        this.isOperationDone = false;
+        this.isOperatorAsigned = false;
+        this.showCompleteOperation = false;
         
         this.actualizarUI();
     }
 
     //Resetear los elementos de la interfaz gráfica
     actualizarUI(){
-        this.operand1Element.textContent = this.operand1 + this.operator;
+        
+        if(this.showCompleteOperation){
+            this.operand1Element.textContent = this.operand1 + this.operator + this.result + '=';
+            
+            this.showCompleteOperation = false;
+        }
+        else{
+            this.operand1Element.textContent = this.operand1 + this.operator;
+        }
+
         this.operand2Element.textContent = this.operand2;
+
+        
     }
 
     agregarNumero(num){
         //Comprobar que solo se pueda poner un punto para cifras decimales
         if(num=='.' && this.operand2.includes('.')) return;
 
-        //Empleamos una estructura condicional para que no se mantenga el cero inicial en la interfaz
-        if(this.operand2 == 0){
+      
+        if(this.isOperationDone){           //Ejecutar en caso de que se haya realizado previamente una operación
+            this.operand1 = this.operand2;
+            this.operand2 = num;
+            this.isOperationDone = false;
+        }  
+        else if(this.isOperatorAsigned){
+            this.operand2 = num;
+            this.isOperatorAsigned = false;
+        }
+        else if(this.operand2 == '0' && num != '.'){             //Empleamos una estructura condicional para que no se mantenga el cero inicial en la interfaz
             this.operand2 = num;
         }
         else{
-            //Se concatena el número seleccionado al operando 2
-            this.operand2 = this.operand2.toString() + num;
+            this.operand2 = this.operand2.toString() + num;         //Se concatena el número seleccionado al operando 2
         }
 
         this.actualizarUI();
     }
 
+    borrarNumero(){
+        if(this.operand2 !== 0){
+            this.operand2 = +this.operand2.toString().slice(0,-1);
+            this.actualizarUI();
+        }
+        
+    }
+
     asignarOperador(operator){
         this.operator = operator;
         this.operand1 = this.operand2;
-        this.operand2 = 0;
+        //this.operand2 = 0;
+        this.isOperatorAsigned = this.isOperationDone == true? false : true;
+
+        this.showCompleteOperation = false;
 
         this.actualizarUI();
 
     }
 
     resultado(){
-        switch(operator){
-            
-        }
-    }
+        this.result = this.operand2;
 
+        //En función del valor de la variable operator se determina el valor mostrado.
+        switch(this.operator){
+            case '+':
+                this.operand2 = +this.operand1 + +this.operand2;
+                break;
+            case '-':
+                this.operand2 = +this.operand1 - +this.operand2;
+                break;
+            case '*':
+                this.operand2 = +this.operand1 * +this.operand2;
+                break;
+            case '/':
+                this.operand2 = +this.operand2 != 0? +this.operand1 / +this.operand2 : "INFINITY";
+                break;
+            default:
+                return;
+        }
+        this.isOperationDone = true;
+        this.showCompleteOperation = true;
+
+        this.actualizarUI();
+    }
 }
 
 const operand1Element = document.querySelector("[data-operand-1]");
 const operand2Element = document.querySelector("[data-operand-2]");
 const clearButton = document.querySelector("[data-clear-button]");
 const equalButton = document.querySelector("[data-equal]");
+const deleteButton = document.querySelector("[data-delete-button]");
 const numberButtons = document.querySelectorAll("[data-number]"); //Array que recoge todos los elementos de botón numérico
 const operatorButtons = document.querySelectorAll("[data-operator]");  //Array que recoge todos los elementos de botón de operador
 
@@ -86,4 +140,8 @@ operatorButtons.forEach((button)=>{
 
 equalButton.addEventListener("click", () =>{
     calculadora.resultado();
+});
+
+deleteButton.addEventListener("click", ()=>{
+    calculadora.borrarNumero();
 });
